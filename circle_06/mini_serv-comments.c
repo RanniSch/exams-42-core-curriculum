718490e0c085a6e175e88dbff456b39266189adf
rschlott <rschlott@student.42wolfsburg.de>
2024-06-29 12:25:59 +0200

    comments.2

--- circle_06/mini_serv-comments.c
+++ circle_06/mini_serv-comments.c
@@ -1,7 +1,7 @@
-#include <sys/socket.h>
+#include <sys/socket.h> 
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
+#include <string.h>				// bzero()
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <netdb.h>
@@ -9,17 +9,22 @@
 typedef struct s_client
 {
 	int id;
-	char msg[1024];
+	char msg[1024];				// Maximum number of client connections allowed
 }				t_client;
 
-t_client	clients[1024];
+t_client	clients[1024];				// Array to store client socket descriptors								
+
+// File descriptor sets: File descriptor sets for tracking socket activity
 fd_set		writeFds;
 fd_set		readFds;
 fd_set		active;
 int 		fdMax = 0;
-int			idNext = 0;
-char		bufferRead[120000], bufferWrite[120000];
+int			idNext = 0;				// Identifier for the next client connection
 
+// Buffer for storing received messages
+char		bufferRead[120000], bufferWrite[120000];				// Size of the buffer used for message exchange
+
+// takes care of all error management
 void	ft_error(char *string)
 {
 	if (string)
@@ -27,7 +32,7 @@ void	ft_error(char *string)
 	else
 		write(2, "Fatal error", strlen("Fatal error"));
 	write(2, "\n", 1);
-	exit(1);
+	exit(1);										// Terminate the program with a non-zero status code	
 }
 
 // This part is reusing the send utility for each section.
@@ -40,63 +45,81 @@ void	send_all(int fd)
 
 int main(int argc, char **argv)
 {
-	if (argc != 2)
-		ft_error("Wrong number of arguments");
+	if (argc != 2)									// Check if the number of command line arguments is incorrect
+		ft_error("Wrong number of arguments");		// Print an error message with ft_error
 
-	int socketfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (socketfd < 0)
-		ft_error(NULL);
+	// Create the server socket
+	int socketfd = socket(AF_INET, SOCK_STREAM, 0);				// Create a socket with IPv4 addressing and TCP protocol
+	if (socketfd < 0)				// Check if socket creation failed			
+		ft_error(NULL);								// Print an error message with ft_error
 	
-	FD_ZERO(&active);
-	bzero(&clients, sizeof(clients));
-	fdMax = socketfd;
-	FD_SET(socketfd, &active);
+	// Initialise the active sockets set
+	FD_ZERO(&active);				// Clear the set of active sockets
+	bzero(&clients, sizeof(clients));				// place in the sizeof 'clients' zero-valued bytes in the area pointed to by '&clients'
+	fdMax = socketfd;				// Variable to track the maximum socket descriptor
+	FD_SET(socketfd, &active);		// Add the server socket to the set
 
-	struct sockaddr_in  serveraddr;
+	// Set up the server address
+	struct sockaddr_in  serveraddr;				// Structure to hold the server address
 	socklen_t           len;
-	bzero(&serveraddr, sizeof(serveraddr));
+	bzero(&serveraddr, sizeof(serveraddr));	// place in the sizeof 'serveraddr' zero-valued bytes in the area pointed to by '&serveraddr'; probably similar to struct sockaddr_in serveraddr = {0};
 
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = htonl(2130706433);
-	serveraddr.sin_port = htons(atoi(argv[1]));
+	serveraddr.sin_family = AF_INET;		// Set address family to IPv4
+	/** check for 2130706433, as it has to stand for this machine. Instead INADDR_LOOPBACK could be an option **/
+	serveraddr.sin_addr.s_addr = htonl(2130706433);		// Set the IP address to localhost   
+	serveraddr.sin_port = htons(atoi(argv[1]));		// Set the port number from the command line argument
 
+	// Bind the server socket to the specified address
 	if ((bind(socketfd, (const struct sockaddr *) &serveraddr, sizeof(serveraddr))) < 0)
-		ft_error(NULL);
+		ft_error(NULL);		// if binding fails: Print an error message with ft_error
 
-	if (listen(socketfd, 10) < 0)
-		ft_error(NULL);
+	// Listen for incoming connections
+	if (listen(socketfd, 10) < 0)		// 10 are 'MAX_CLIENTS', others have defined it on 128
+		ft_error(NULL);		// if listening fails: Print an error message with ft_error
 
 	while(1)
 	{
-		readFds = writeFds = active;
+		// Wait for activity on the sockets
+		readFds = writeFds = active;				// Copy the active sockets 'set' for use with select()
 		if (select(fdMax + 1, &readFds, &writeFds, NULL, NULL) < 0)
-			continue;
+			continue;		// if select() fails: jumps directly to while(1), so no break, but the input is ignored. 
+		
+		// Check each socket for activity
 		for(int fdI = 0; fdI <= fdMax; fdI++)
 		{
 			if (socketfd < 0)
 				ft_error(NULL);
-			if (FD_ISSET(fdI, &readFds) && fdI == socketfd)
+			if (FD_ISSET(fdI, &readFds) && fdI == socketfd)		// fdI == socketfd checks if the activity is on the server socket
 			{
-				// This is where we accept connections.
-				int connfd = accept(socketfd, (struct sockaddr *)&serveraddr, &len);
+				// This is where we accept connections; new client connection
+				int connfd = accept(socketfd, (struct sockaddr *)&serveraddr, &len);		// Accept a new client connection
 				if (connfd < 0)
-					continue;
-				fdMax = connfd > fdMax ? connfd : fdMax;
-				clients[connfd].id = idNext++;
-				FD_SET(connfd, &active);
-				sprintf(bufferWrite, "server: client %d just arrived\n", clients[connfd].id);
-				send_all(connfd);
+					continue;		// if accepting fails: jumps directly to for(int fdI = 0; fdI <= fdMax; fdI++), so no break, but the input is ignored.
+				
+				// Add the new client socket to the active set
+				fdMax = connfd > fdMax ? connfd : fdMax;		// Update the maximum socket descriptor
+				clients[connfd].id = idNext++;		// Store the client socket for future reference; Add the client socket to the array	
+				FD_SET(connfd, &active);		// Add the client socket to the set of active sockets
+				
+				// Send a welcome message to the client
+				sprintf(bufferWrite, "server: client %d just arrived\n", clients[connfd].id);		// Prepare the welcome message
+				send_all(connfd);		// Send the welcome message to the client with send_all()
 				break;
 			}
-			if (FD_ISSET(fdI, &readFds) && fdI != socketfd)
+			if (FD_ISSET(fdI, &readFds) && fdI != socketfd)			// if the if before does not apply: if the activity is not on the server socket
 			{
-				int res = recv(fdI, bufferRead, 65536, 0);
+				// Data received from a client
+				/** check for 65536. Instead sizeof(bufferRead) - 1 could be an option **/
+				int res = recv(fdI, bufferRead, 65536, 0);		// Receive data from the client
 				if (res <= 0)
 				{
-					sprintf(bufferWrite, "server: client %d just left\n", clients[fdI].id);
-					send_all(fdI);
-					FD_CLR(fdI, &active);
-					close(fdI);
+					// Client disconnected
+					sprintf(bufferWrite, "server: client %d just left\n", clients[fdI].id);		// Prepare the disconnection message
+					send_all(fdI);				// Send the disconneczion message to the client with send_all()
+					
+					// Close the socket and remove it from the active set
+					FD_CLR(fdI, &active);		// Remove the client socket from the set of active sockets
+					close(fdI);		// Close the client socket
 					break;
 				}
 				else
@@ -106,9 +129,10 @@ int main(int argc, char **argv)
 						clients[fdI].msg[j] = bufferRead[i];
 						if (clients[fdI].msg[j] == '\n')
 						{
-							clients[fdI].msg[j] = '\0';
-							sprintf(bufferWrite, "client %d: %s\n", clients[fdI].id, clients[fdI].msg);
-							send_all(fdI);
+							// Broadcast the received message to all other clients
+							clients[fdI].msg[j] = '\0';		// Null-terminate the received message
+							sprintf(bufferWrite, "client %d: %s\n", clients[fdI].id, clients[fdI].msg);		// Add client identifier to the message
+							send_all(fdI);		// // Send the message to other clients???
 							bzero(&clients[fdI].msg, strlen(clients[fdI].msg));
 							j = -1;
 						}
